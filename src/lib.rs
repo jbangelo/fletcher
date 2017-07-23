9c345536fe4f329a34b4bf17036ba57464cd1597
Joseph Angelo <b.j.angelo@gmail.com>
2017-07-23 15:31:31 -0700

    Added an initial test of the 16 bit algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,12 @@ pub mod fletcher32;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {}
+    fn fletcher16_test() {
+        let data1 = vec![b'a', b'b', b'c', b'd', b'e'];
+        let mut checksum = ::fletcher16::Fletcher16::new();
+
+        checksum.update(&data1);
+        println!("{}", checksum.value());
+        assert!(checksum.value() == 51440);
+    }
 }
