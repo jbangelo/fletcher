da63c50fc036dafb7d569d477ed582c2a6e985c6
Joseph Angelo <b.j.angelo@gmail.com>
2017-07-23 20:56:29 -0700

    Updated 16 bit and added a test for 32 bit

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,22 @@ pub mod fletcher32;
 mod tests {
     #[test]
     fn fletcher16_test() {
-        let data1 = vec![b'a', b'b', b'c', b'd', b'e'];
-        let mut checksum = ::fletcher16::Fletcher16::new();
+        let data = vec![0xC1, 0x77, 0xE9, 0xC0, 0xAB, 0x1E];
+        let mut checksum = super::fletcher16::Fletcher16::new();
 
-        checksum.update(&data1);
+        checksum.update(&data);
         println!("{}", checksum.value());
-        assert!(checksum.value() == 51440);
+        assert!(checksum.value() == 0x3fad);
+
+    }
+
+    #[test]
+    fn fletcher32_test() {
+        let data = vec![0xF02A, 0xCB0D, 0x5639, 0x6501, 0x2384, 0x75BB];
+        let mut checksum = super::fletcher32::Fletcher32::new();
+
+        checksum.update(&data);
+        println!("{}", checksum.value());
+        assert!(checksum.value() == 0xdcf30fb3);
     }
 }
