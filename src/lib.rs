8175c8873fc42ebc670d828ba89d71c9af1f344f
Joseph Angelo <joseph.angelo@swift-nav.com>
2022-09-19 18:46:39 -0700

    Move documentation to code, leave smaller README

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,68 @@
-#![doc = include_str!("../README.md")]
+//! Fletcher's Checksum was developed to provide nearly the same error checking
+//! capability as a CRC but with a faster software implementation. This is not a
+//! cryptographically secure checksum, it's only meant to be used for checking
+//! the integrity of data NOT the authenticity.
+//!
+//! ## Algorithm Pros
+//! This algorithm is faster to run in software than most CRCs. This is because the
+//! CRC algorithm was originally designed to be simple to implement in hardware, but
+//! not neccesarily in software. The Fletcher Checksum was designed specifically to
+//! be suited for implementation in software.
+//!
+//! ## Algorithm Cons
+//! This checksum algorithm does suffer from not being able to distinguish `0xFF`
+//! from `0x00`. Meaning a block of data with all bits set to 1 will have the exact
+//! same the same checksum as a block of data with all bits set to 0. This comes
+//! from the fact that the algorithm uses one's complement math.
+//!
+//! Fletcher's checksum isn't quite as good at detecting bit errors in data as a CRC
+//! with a well chosen polynomial.
+//!
+//! # How To Use
+//! If you have an entire block of data you want to get the checksum of you can
+//! use the calc functions ([`calc_fletcher16`], [`calc_fletcher32`], [`calc_fletcher64`])
+//! to get the checksum in a single function call.
+//!
+//! If you are getting the data in chunks you can make a [`Fletcher`] object
+//! (e.g. [`Fletcher16`], [`Fletcher32`], [`Fletcher64`]) to  manage the intermediate
+//! state between chunks of data. The checksum objects take in slices of data to
+//! process. There is no minimum length required of the slices, all of the provided
+//! data will be processed. The type of the input data is dictated by  the size of the
+//! checksum value. i.e. a 64-bit checksum operates on 32-bit wide values.
+//!
+//! The checksum object can be queried for it's current checksum value at any
+//! time with the [`Fletcher::value()`] function.
+//!
+//! ## Check Values
+//! An alternative way to use the Fletcher checksum is to append a value to the
+//! end of you data stream so that the final checksum produced comes out to zero.
+//! This doesn't implicitly happen when appending the checksum value to the end
+//! of your data, you must derive a specific value from the checksum. This can be
+//! done with the [`Fletcher::check_values()`] function, or the crate level functions
+//! if you have all of the data available ([`checkvalues_fletcher16`],
+//! [`checkvalues_fletcher32`], [`checkvalues_fletcher64`]).
+//!
+//! ## Examples
+//! ```rust
+//! let data: [u8; 6] = [0xC1, 0x77, 0xE9, 0xC0, 0xAB, 0x1E];
+//! assert_eq!(fletcher::calc_fletcher16(&data), 0x3FAD);
+//! // Or if you want to work on smaller chunks of data
+//! let mut checksum = fletcher::Fletcher16::new();
+//! checksum.update(&data[0..3]);
+//! checksum.update(&data[3..]);
+//! assert_eq!(checksum.value(), 0x3FAD);
+//! ```
+//!
+//! Calculating and appending check values:
+//! ```rust
+//! use fletcher::calc_fletcher16;
+//! let mut data = vec![0xC1, 0x77, 0xE9, 0xC0, 0xAB, 0x1E, 0x00, 0x00];
+//! let check_values = fletcher::checkvalues_fletcher16(&data);
+//! data.push(check_values[0]);
+//! data.push(check_values[1]);
+//! assert_eq!(calc_fletcher16(&data), 0);
+//! ```
+
 #![no_std]
 
 #[cfg(test)]
